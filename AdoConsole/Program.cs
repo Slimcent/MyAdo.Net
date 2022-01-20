5cfc2eacde889585876578db52931ee15aacb727
Slimcent <33299753+Slimcent@users.noreply.github.com>
2022-01-20 19:14:12 +0100

    Modified Program.cs

--- AdoConsole/Program.cs
+++ AdoConsole/Program.cs
@@ -16,11 +16,11 @@ namespace AdoConsole
             var connectionString = ConfigurationManager.ConnectionStrings["AdoConnect"].ConnectionString;
             var db = new AdoDal();
 
-            /*var users = db.GetAllUsers();    // Get all Users
+            var users = db.GetAllUsers();    // Get all Users
             foreach (var user in users)
             {
                 Console.WriteLine($"Id: {user.Id}\nName: {user.Name}\nEmail: {user.Email}");
-            }*/
+            }
 
             /*var accounts = db.GetAllAccounts();    // Get allAccounts
             foreach (var acc in accounts)
@@ -33,17 +33,17 @@ namespace AdoConsole
             //db.KeepingTrack(10, new MyAdoDal.Models.RollCheck { Name = "", Email = "" });
 
            
-            bool throwEx = true;
-            Console.Write("Do you want to throw an exception (Y or N): ");
-            var userAnswer = Console.ReadLine();
-            if (userAnswer?.ToLower() == "n")
-            {
-                throwEx = false;
-            }
+            //bool throwEx = true;
+            //Console.Write("Do you want to throw an exception (Y or N): ");
+            //var userAnswer = Console.ReadLine();
+            //if (userAnswer?.ToLower() == "n")
+            //{
+            //    throwEx = false;
+            //}
             
-            // Process customer 1 – enter the id for the customer to move.
-            db.ProcessTracking(throwEx, 1000);
-            Console.WriteLine("Check RollCheck table for results");
+            //// Process customer 1 – enter the id for the customer to move.
+            //db.ProcessTracking(throwEx, 1000);
+            //Console.WriteLine("Check RollCheck table for results");
 
             //Console.ReadLine();
         }
